045f7e407a18b63bee644fd9b427f5ba0279610d
tangtyk <tyklww@163.com>
2017-10-30 10:14:39 +0800

    update proxy

--- src/main.js
+++ src/main.js
@@ -10,8 +10,7 @@ Vue.config.productionTip = false
 
 Vue.use(VueLazyload)
 Vue.use(VueResource)
-// Vue.prototype.$api = process.env.NODE_ENV === 'production' ? 'http://59.110.240.8:8888/Goal' : '/api'
-Vue.prototype.$api = '/api'
+Vue.prototype.$api = process.env.NODE_ENV === 'production' ? '/apis/Goal' : '/api'
 
 /* eslint-disable no-new */
 new Vue({
